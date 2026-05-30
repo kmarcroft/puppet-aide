ede3255d03e847db323286ec2b704b517b38c116
Kristian M. <12916346+kmarcroft@users.noreply.github.com>
2026-05-30 02:10:07 +0200

    Add support for Hiera-defined rules and watches in AIDE configuration
    - Introduced `aide::rule` and `aide::watch` resources in Hiera for dynamic rule and watch management.
    - Updated `common.yaml` to include new keys for rules and watches.
    - Enhanced `init.pp`, `rule.pp`, and `watch.pp` with detailed parameter documentation and examples.
    - Added tests for Hiera integration in `aide_spec.rb`.

--- spec/classes/aide_spec.rb
+++ spec/classes/aide_spec.rb
@@ -12,6 +12,38 @@ describe 'aide' do
       it { is_expected.to contain_class('aide::cron').that_requires('Package[aide]') }
       it { is_expected.to contain_class('aide::config').that_requires('Package[aide]') }
       it { is_expected.to contain_class('aide::firstrun').that_requires('Package[aide]') }
+
+      context 'with rules declared via Hiera' do
+        let(:hiera_data) do
+          {
+            'aide::rule' => {
+              'MYRULE' => { 'rules' => ['p', 'i', 'n'] },
+              'PERMS'  => { 'rules' => ['p', 'u', 'g'], 'order' => '04' },
+            },
+          }
+        end
+
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_aide__rule('MYRULE').with_rules(['p', 'i', 'n']) }
+        it { is_expected.to contain_aide__rule('PERMS').with_rules(['p', 'u', 'g']).with_order('04') }
+      end
+
+      context 'with watches declared via Hiera' do
+        let(:hiera_data) do
+          {
+            'aide::watch' => {
+              '/etc'     => { 'rules' => ['NORMAL'] },
+              '/var/log' => { 'rules' => ['LOG'], 'type' => 'regular' },
+              '/tmp'     => { 'type' => 'exclude' },
+            },
+          }
+        end
+
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_aide__watch('/etc').with_rules(['NORMAL']) }
+        it { is_expected.to contain_aide__watch('/var/log').with_rules(['LOG']).with_type('regular') }
+        it { is_expected.to contain_aide__watch('/tmp').with_type('exclude') }
+      end
     end
   end
 end
